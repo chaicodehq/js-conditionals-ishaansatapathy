8b4364a1d1dbdd82279eb52475c24f81ae43c6fa
ishaansatapathy <94060148+ishaansatapathy@users.noreply.github.com>
2026-02-08 01:17:37 +0530

    quesn 1 and quesn 2 done

--- src/01-ticket-pricing.js
+++ src/01-ticket-pricing.js
@@ -23,5 +23,38 @@
  * @returns {number} The ticket price, or -1 for invalid input
  */
 export function getTicketPrice(age, isWeekend) {
-  // Your code here
+  if(age<0||isNaN(age)){
+    return -1;
+  }
+  if(isWeekend){
+    if(age>=0 && age<=12){
+      return 11;
+   
+    }
+    else if(age>=13 && age<=17){
+      return 15;
+    }
+    else if(age>=18 && age<=59){
+      return 18;
+    }
+    else if(age>=60){
+      return 13;
+    }
+  }else{
+     if(age>=0 && age<=12){
+      return 8;
+   
+    }
+    else if(age>=13 && age<=17){
+      return 12;
+    }
+    else if(age>=18 && age<=59){
+      return 15;
+    }
+    else if(age>=60){
+      return 10;
+    }
+
+  }
+  return -1;
 }
